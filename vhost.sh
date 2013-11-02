16720423feae93be3f68fe7f7e9245bbe1f06aba
Thiago Moreira <loganguns@gmail.com>
2013-11-02 20:43:42 -0200

    First Commit

---
+++ vhost.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+###########################################################
+# Criado por Adler Parnas <adler.parnas@doisdeum.com.br>  #
+# 2011-02-23                                              #
+#                                                         #
+# Editado por Thiago Moreira <loganguns@gmail.com>        #
+# 2013-11-2                                               #
+#                                                         #
+###########################################################
+#                                                         #
+# Script para criar um virtual host no apache e adicionar #
+# o nome do host no arquivo hosts                         #
+#                                                         #
+###########################################################
+
+echo "Informe o nome do server (Ex.: adler) :"
+read server
+
+echo "Informe o caminho do site (Ex.: /var/www/adler) :"
+read path
+
+echo "Criando configuração de VHost para o server"
+
+echo "
+<VirtualHost *:80>
+    ServerAdmin webmaster@localhost
+    ServerName $server.dev
+    ServerAlias www.$server.dev
+
+    DocumentRoot "$path"
+
+    <Directory "$path">
+        Options Indexes FollowSymLinks MultiViews
+        AllowOverride All
+        Order allow,deny
+        Allow from all
+    </Directory>
+</VirtualHost>
+" > /etc/apache2/sites-available/$server.conf
+
+echo "Ativando VHOST $server"
+ln -s /etc/apache2/sites-available/$server.conf /etc/apache2/sites-enabled/$server.conf
+# a2ensite $server
+
+echo "Atualizando arquivo hosts"
+echo "127.0.0.1 $server.dev www.$server.dev" >> /etc/hosts
+
+echo "Reiniciando apache";
+service apache2 restart
+
+echo "VHOST criado";
